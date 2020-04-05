3832de89334410475b46d1d3d01e1a883084d391
What The Cloud <admin@whatthecloud.tech>
2020-04-05 10:20:12 -0500

    Docker logging

--- Docker/dump.sh
+++ Docker/dump.sh
@@ -24,7 +24,7 @@ output_gen() {
 cipher_command() {
   if [[ -n "${CIPHER_ALGORITHM}" ]]; then
     if [[ -z "${CIPHER_PASSWORD}" ]]; then
-      echo "The value of CIPHER_PASSWORD must be set if enc"
+      echo "The value of CIPHER_PASSWORD must be set if enc" >&2
       exit
     fi
     openssl ${CIPHER_ALGORITHM} -salt -pbkdf2 -k ${CIPHER_PASSWORD}
@@ -35,43 +35,43 @@ build_output() {
 	OUTPUT_FULL=$(output_gen $1)
 	if [[ -z "${CIPHER_ALGORITHM}" ]]; then
 		db_dump | cat - > "${OUTPUT_FULL}"
-		echo "Completed: Exported ${OUTPUT_FULL}"
+		echo "Completed: Exported ${OUTPUT_FULL}" 2>&1
 	else
 		db_dump | cipher_command | cat - > "${OUTPUT_FULL}"
-		echo "Completed: Exported ${OUTPUT_FULL}"
+		echo "Completed: Exported ${OUTPUT_FULL}" 2>&1
 	fi
 }
 
 if [[ -z "${DB_USER}" ]]; then
-	echo "Missing DB_USER env variable"
+	echo "Missing DB_USER env variable" >&2
 	exit 1
 fi
 if [[ -z "${DB_PASS}" ]]; then
-	echo "Missing DB_PASS env variable"
+	echo "Missing DB_PASS env variable" >&2
 	exit 1
 fi
 if [[ -z "${DB_HOST}" ]]; then
-	echo "Missing DB_HOST env variable"
+	echo "Missing DB_HOST env variable" >&2
 	exit 1
 fi
 if [[ -z "${ALL_DATABASES}" ]]; then
 	if [[ -z "${DB_NAME}" ]]; then
-		echo "Missing DB_NAME env variable"
+		echo "Missing DB_NAME env variable" >&2
 		exit 1
 	fi
 	if [[ -z "${DB_PORT}" ]]; then
 		db_dump() {
 			"mysqldump --user=${DB_USER} --password=${DB_PASS} --host=${DB_HOST} $@ ${DB_NAME}"
 			}
-		echo "Created DB_DUMP command for ${DB_NAME}"
-		echo "Calling build command"
+		echo "Created DB_DUMP command for ${DB_NAME}" 2>&1
+		echo "Calling build command" 2>&1
 		build_output ${DB_NAME}
 	else
 		db_dump() {
 			"mysqldump --user=${DB_USER} --password=${DB_PASS} --host=${DB_HOST} --port=${DB_PORT} $@ ${DB_NAME}"
 			}
-		echo "Created DB_DUMP command for ${DB_NAME}"
-		echo "Calling build command"
+		echo "Created DB_DUMP command for ${DB_NAME}" 2>&1
+		echo "Calling build command" 2>&1
 		build_output ${DB_NAME}
 	fi
 else
@@ -83,14 +83,14 @@ else
 				db_dump() {
 					"mysqldump --user=${DB_USER} --password=${DB_PASS} --host=${DB_HOST} --databases $db" 
 					}
-				echo "Created DB_DUMP command for $db"
+				echo "Created DB_DUMP command for $db" 2>&1
 			else
 				db_dump() {
 					"mysqldump --user=${DB_USER} --password=${DB_PASS} --host=${DB_HOST} --port=${DB_PORT} --databases $db"
 					}
-				echo "Created DB_DUMP command for $db"
+				echo "Created DB_DUMP command for $db" 2>&1
 			fi
-			echo "Calling build command"
+			echo "Calling build command" 2>&1
 			build_output "$db"
 		fi
 	done
